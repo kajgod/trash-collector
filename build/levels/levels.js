66cbafbb93f64222f31a41a3e443742dc7d9d9ea
kajgod <kajgod@gmail.com>
2021-10-29 16:53:05 +0200

    New level by Sergej

--- build/levels/levels.js
+++ build/levels/levels.js
@@ -1,6 +1,7 @@
 import level1 from "./1";
 import level2 from "./2";
 import level3 from "./3";
-const levels = [level1, level2, level3];
+import level4 from "./4";
+const levels = [level1, level2, level3, level4];
 export default levels;
 //# sourceMappingURL=levels.js.map
--- src/levels/levels.ts
+++ src/levels/levels.ts
@@ -1,7 +1,8 @@
 import level1 from "./1";
 import level2 from "./2";
 import level3 from "./3";
+import level4 from "./4";
 
-const levels: string[] = [level1, level2, level3];
+const levels: string[] = [level1, level2, level3, level4];
 
 export default levels;
